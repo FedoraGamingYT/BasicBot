354bca533cb5eabea405cdbf9891daa6294cb41c
FedoraGamingYT <38592294+stanislav123456@users.noreply.github.com>
2021-04-25 20:55:12 +0200

    added better help cmd and not working setverifychannel cmd yet will update in next rleaese

--- commands/arg.js
+++ commands/arg.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js")
+const config = require("../config.json")
 module.exports = {
     name: "arg",
     description: "Gets the arg you runned after your command.",
@@ -9,11 +10,13 @@ const embed = new Discord.MessageEmbed()
     .setTitle("The arg you provided")
     .setDescription(`Your argument was "${arg1}"`)
     .setColor('GREEN')
+    .setFooter(config.botname)
 const failed = new Discord.MessageEmbed()
     .setTitle("Command failed!")
     .setDescription(`No argument was provided!`)
     .setColor('RED')
+    .setFooter(config.botname)
     if(!arg1) return message.channel.send(failed);
-    message.channel.send(embed)
+    message.channel.send(embed)   
 }
 }
